3e5558ba1a020f76d4e9bb491f92234ba869ded5
nishant bodade <nishantbodade@gmail.com>
2026-02-28 14:14:12 +0530

    first tasklet step

--- First-Spring-Batch-Job-with-Tasklet-Step/src/main/java/com/infybuzz/config/SampleJob.java
+++ First-Spring-Batch-Job-with-Tasklet-Step/src/main/java/com/infybuzz/config/SampleJob.java
@@ -14,35 +14,41 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SampleJob {
-
+	
 	@Autowired
 	private JobBuilderFactory jobBuilderFactory;
-
+	
 	@Autowired
 	private StepBuilderFactory stepBuilderFactory;
-
+	
 	@Bean
 	public Job firstJob() {
-		return jobBuilderFactory.get("First Job")
-				.start(firstStep())
-				.build();
+		return jobBuilderFactory.get("First Job").start(firstStep())
+		.build();
 	}
-
-	private Step firstStep() {
+	
+	@Bean
+	public Step firstStep() {
+		
 		return stepBuilderFactory.get("First Step")
-				.tasklet(firstTask())
-				.build();
+		.tasklet(firstTask())
+		.build();
+		
 	}
-
-	private Tasklet firstTask() {
+	
+	public Tasklet firstTask() {
 		return new Tasklet() {
-
+			
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("This is first tasklet step");
+				System.out.println("This is first Tasklet Step");
 				return RepeatStatus.FINISHED;
 			}
 		};
+		
 	}
+	
+
+	
 
 }

89bc995ffac4a457836993beac8c63b9d7b4e87b
nishant bodade <nishantbodade@gmail.com>
2026-03-08 15:46:48 +0530

    Stopping a Spring Batch Job

--- First-Spring-Batch-Job-with-Tasklet-Step/src/main/java/com/infybuzz/controller/JobController.java
+++ First-Spring-Batch-Job-with-Tasklet-Step/src/main/java/com/infybuzz/controller/JobController.java
@@ -8,7 +8,10 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobExecutionNotRunningException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.NoSuchJobExecutionException;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
@@ -29,18 +32,26 @@ public class JobController {
 
 	@Autowired
 	JobService jobService;
+	
+	@Autowired
+	JobOperator jobOperator;
 
 	@GetMapping("/start/{jobName}")
-	public String startJob(@PathVariable String jobName,
-			@RequestBody List<JobParamsRequest> request
-			
-			) throws Exception {
-		
-		
+	public String startJob(@PathVariable String jobName, @RequestBody List<JobParamsRequest> request
 
-		jobService.startJob(jobName,request);
+	) throws Exception {
+
+		jobService.startJob(jobName, request);
 		return "Job started...!";
 
+	}
+	
+	@GetMapping("/stop/{jobExecutionId}")
+	public String stopJob(long jobExecutionId) throws Exception {
+		
+		jobOperator.stop(jobExecutionId);
+		return "job stopped...!";
+		
 	}
 
 }

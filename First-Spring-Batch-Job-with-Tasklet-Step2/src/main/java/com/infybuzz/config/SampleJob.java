03862c96dd4ac25c6fae7c831d4802ebc7fba46b
nishant bodade <nishantbodade@gmail.com>
2026-03-09 17:08:05 +0530

    set delimiter : two way to to this

--- First-Spring-Batch-Job-with-Tasklet-Step2/src/main/java/com/infybuzz/config/SampleJob.java
+++ First-Spring-Batch-Job-with-Tasklet-Step2/src/main/java/com/infybuzz/config/SampleJob.java
@@ -4,28 +4,21 @@ import java.io.File;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 
-import com.infybuzz.listener.FirstJobListener;
-import com.infybuzz.listener.FirstStepListener;
 import com.infybuzz.mode.StudentCsv;
 import com.infybuzz.processor.FirstItemProcessor;
 import com.infybuzz.reader.FirstItemReader;
-import com.infybuzz.service.SecondTasklet;
 import com.infybuzz.writer.FirstItemWriter;
 
 @Configuration
@@ -69,10 +62,11 @@ public class SampleJob {
 
 		flatFileItemReader.setLineMapper(new DefaultLineMapper<StudentCsv>() {
 			{
-				setLineTokenizer(new DelimitedLineTokenizer() {
+				setLineTokenizer(new DelimitedLineTokenizer("|") {
 
 					{
 						setNames("Id", "First Name", "Last Name", "Email");
+						//setDelimiter("|");
 					}
 
 				});

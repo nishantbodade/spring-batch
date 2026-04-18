1882062f04385fdccf1c3ce26d291d0c0e8e2563
nishant bodade <nishantbodade@gmail.com>
2026-04-19 00:24:27 +0530

    chunk process project setup

---
+++ chunkProcess/src/main/java/com/springbatch/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.springbatch.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.springbatch.controller","com.springbatch.config"})
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("This is a Test");
+	}
+}

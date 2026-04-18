2c32b5baf6e73babfbbe2aa95ed8c089ff486e0f
nishant bodade <nishantbodade@gmail.com>
2026-04-18 22:29:04 +0530

    multipule step execution in one job

--- demo/src/main/java/com/springbatch/config/BatchConfiguration.java
+++ demo/src/main/java/com/springbatch/config/BatchConfiguration.java
@@ -23,7 +23,7 @@ public class BatchConfiguration {
 	public StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public Step firstStep() {
+	public Step step1() {
 		return this.stepBuilderFactory.get("step1").tasklet(new Tasklet() {
 
 			@Override
@@ -33,9 +33,37 @@ public class BatchConfiguration {
 			}
 		}).build();
 	}
+	
+	@Bean
+	public Step step2() {
+		return this.stepBuilderFactory.get("step1").tasklet(new Tasklet() {
+
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("step2 executed!!");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
+	}
+	
+	@Bean
+	public Step step3() {
+		return this.stepBuilderFactory.get("step3").tasklet(new Tasklet() {
+
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("step3 executed!!");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
+	}
 
 	@Bean
 	public Job firstJob() {
-		return this.jobBuilderFactory.get("job1").start(firstStep()).build();
+		return this.jobBuilderFactory.get("job1")
+				.start(step1())
+				.next(step2())
+				.next(step3())
+				.build();
 	}
 }

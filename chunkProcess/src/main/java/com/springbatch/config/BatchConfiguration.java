8c3e9bd01767e330b008d852dfe4a5456142193d
nishant bodade <nishantbodade@gmail.com>
2026-04-19 14:03:18 +0530

    csv flat file item reader impl

--- chunkProcess/src/main/java/com/springbatch/config/BatchConfiguration.java
+++ chunkProcess/src/main/java/com/springbatch/config/BatchConfiguration.java
@@ -13,11 +13,17 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
+import com.springbatch.domain.Product;
+import com.springbatch.domain.ProductFieldSetMapper;
 import com.springbatch.reader.ProductNameItemReader;
 
 @Configuration
@@ -28,10 +34,10 @@ public class BatchConfiguration {
 
 	@Autowired
 	public StepBuilderFactory stepBuilderFactory;
-	
+
 	@Bean
-	public ItemReader<String> itemReader(){
-		List<String> list=new ArrayList<String>();
+	public ItemReader<String> itemReader() {
+		List<String> list = new ArrayList<String>();
 		list.add("Product 1");
 		list.add("Product 2");
 		list.add("Product 3");
@@ -40,21 +46,34 @@ public class BatchConfiguration {
 		return new ProductNameItemReader(list);
 	}
 
+	public ItemReader<Product> flatFileItemReader() {
+		FlatFileItemReader<Product> fileItemReader = new FlatFileItemReader<Product>();
+		fileItemReader.setLinesToSkip(1);
+		fileItemReader.setResource(new ClassPathResource("/data/Product_Details.csv"));
+
+		DefaultLineMapper<Product> lineMapper = new DefaultLineMapper<Product>();
+		DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+		lineTokenizer.setNames("product_id", "product_name", "product_category", "product_price");
+		lineMapper.setLineTokenizer(lineTokenizer);
+		lineMapper.setFieldSetMapper(new ProductFieldSetMapper());
+		fileItemReader.setLineMapper(lineMapper);
+		return fileItemReader;
+
+	}
+
 	@Bean
 	public Step step1() {
-		return this.stepBuilderFactory.get("chunkBaseStep1")
-				.<String,String>chunk(2)
-				.reader(itemReader())
-				.writer(new ItemWriter<String>(){
+		return this.stepBuilderFactory.get("chunkBaseStep1").<Product, Product>chunk(2).reader(flatFileItemReader())
+				.writer(new ItemWriter<Product>() {
 
 					@Override
-					public void write(List<? extends String> items) throws Exception {
+					public void write(List<? extends Product> items) throws Exception {
 						System.out.println("chunk-processing started");
 						items.forEach(System.out::println);
 						System.out.println("chunk-processing stop");
-						
+
 					}
-					
+
 				}).build();
 	}
 

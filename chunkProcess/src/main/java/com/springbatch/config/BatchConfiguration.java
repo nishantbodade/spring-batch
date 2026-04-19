1bc2ff2fadc0d22d8a5d658eb644178c63102a1c
nishant bodade <nishantbodade@gmail.com>
2026-04-19 13:38:56 +0530

    chunk base item reader impl

--- chunkProcess/src/main/java/com/springbatch/config/BatchConfiguration.java
+++ chunkProcess/src/main/java/com/springbatch/config/BatchConfiguration.java
@@ -1,5 +1,8 @@
 package com.springbatch.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -8,10 +11,14 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.springbatch.reader.ProductNameItemReader;
 
 @Configuration
 @EnableBatchProcessing
@@ -21,17 +28,34 @@ public class BatchConfiguration {
 
 	@Autowired
 	public StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public ItemReader<String> itemReader(){
+		List<String> list=new ArrayList<String>();
+		list.add("Product 1");
+		list.add("Product 2");
+		list.add("Product 3");
+		list.add("Product 4");
+		list.add("Product 5");
+		return new ProductNameItemReader(list);
+	}
 
 	@Bean
 	public Step step1() {
-		return this.stepBuilderFactory.get("step1").tasklet(new Tasklet() {
+		return this.stepBuilderFactory.get("chunkBaseStep1")
+				.<String,String>chunk(2)
+				.reader(itemReader())
+				.writer(new ItemWriter<String>(){
 
-			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("step1 executed!!");
-				return RepeatStatus.FINISHED;
-			}
-		}).build();
+					@Override
+					public void write(List<? extends String> items) throws Exception {
+						System.out.println("chunk-processing started");
+						items.forEach(System.out::println);
+						System.out.println("chunk-processing stop");
+						
+					}
+					
+				}).build();
 	}
 
 	@Bean

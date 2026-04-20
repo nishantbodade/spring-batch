d6b523a460813e40ba47e377750a2cfb37e60425
nishant bodade <nishantbodade@gmail.com>
2026-04-20 16:49:53 +0530

    simple transformation of data using item processor

--- chunkProcess/src/main/java/com/springbatch/processor/MyProductItemProcessor.java
+++ chunkProcess/src/main/java/com/springbatch/processor/MyProductItemProcessor.java
@@ -9,6 +9,8 @@ public class MyProductItemProcessor implements ItemProcessor<Product, Product> {
 	@Override
 	public Product process(Product item) throws Exception {
 		System.out.println("processor executed..!");
+		Integer price=item.getProductPrice();
+		item.setProductPrice((int)(price-((0.1)*1)));
 		return item;
 	}
 
